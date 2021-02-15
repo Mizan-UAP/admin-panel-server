5bec7505f7a49c0200e8db4f1142a0b1bd817baf
mizanurrahman <justmizan2013@gmail.com>
2021-02-15 23:42:28 +0600

    server solved

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
-const app = express()
+
 const MongoClient = require('mongodb').MongoClient;
 // const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const bodyParser = require('body-parser')
 const cors = require('cors');
 
+const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}>@cluster0.djtwh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djtwh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -21,13 +25,21 @@ client.connect(err => {
     const collection = client.db("adminPanel").collection("addProduct");
 
     // send data to db
-    app.post("/addItem", (req, res) => {
+    app.post("/addItems", (req, res) => {
         const item = req.body;
         console.log(item);
         collection.insertOne(item)
             .then(result => {
-                // res.send(result.insertedCount > 0)
-                res.redirect('/')
+                res.send(result.insertedCount > 0)
+
+            })
+    })
+
+    //Getting Data from db 
+    app.get('/items', (req, res) => {
+        collection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
             })
     })
     // client.close();
